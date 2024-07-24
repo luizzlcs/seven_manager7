35e37b79cc861f9cf9b21687ec8736f176079cde
Luiz Carlos <luizzlcs@gmail.com>
2024-07-24 16:24:45 -0300

    Implementando GetIt

--- lib/src/core/injection/injection.dart
+++ lib/src/core/injection/injection.dart
@@ -3,15 +3,17 @@
 import 'dart:developer';
 
 import 'package:get_it/get_it.dart';
+import 'package:seven_manager/src/pages/auth/login/login_controller.dart';
+
+import '../services/firebase/firebase_auth_service.dart';
 
 final getIt = GetIt.I;
 
-void main(){
-  configureDependencies();
-}
+
 void configureDependencies(){
   log('Inicio da configuração de dependência');
-
+    getIt.registerFactory(() => FirebaseAuthService());
+    getIt.registerFactory(() => LoginController(firebaseAuth: getIt<FirebaseAuthService>()));
 
   log('Fim da configuração de dependência');
 
