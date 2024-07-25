ca1bdab03a93a4fa1c4286df1d20504727cc3f16
Luiz Carlos <luizzlcs@gmail.com>
2024-07-25 15:16:43 -0300

    Injeção de dependência no GetIt para as controllers.

--- lib/src/core/injection/injection.dart
+++ lib/src/core/injection/injection.dart
@@ -4,7 +4,7 @@ import 'dart:developer';
 
 import 'package:get_it/get_it.dart';
 import 'package:seven_manager/src/pages/auth/login/login_controller.dart';
-
+import 'package:seven_manager/src/pages/auth/register/register_controller.dart';
 import '../services/firebase/firebase_auth_service.dart';
 
 final getIt = GetIt.I;
@@ -13,6 +13,7 @@ final getIt = GetIt.I;
 void configureDependencies(){
   log('Inicio da configuração de dependência');
     getIt.registerFactory(() => FirebaseAuthService());
+    getIt.registerFactory(()=> RegisterController(firebaseAuth: getIt<FirebaseAuthService>()));
     getIt.registerFactory(() => LoginController(firebaseAuth: getIt<FirebaseAuthService>()));
 
   log('Fim da configuração de dependência');
