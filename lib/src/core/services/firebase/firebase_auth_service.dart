5bcd02bad6f2e20981b582610bb73955b4632292
Luiz Carlos <luizzlcs@gmail.com>
2024-07-26 17:04:01 -0300

    Add method Create user

--- lib/src/core/services/firebase/firebase_auth_service.dart
+++ lib/src/core/services/firebase/firebase_auth_service.dart
@@ -5,6 +5,39 @@ import 'package:firebase_auth/firebase_auth.dart';
 class FirebaseAuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  Future<String?> createUser({
+    required name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      UserCredential userCredential = await _auth
+          .createUserWithEmailAndPassword(email: email, password: password);
+      await userCredential.user!.updateDisplayName(name);
+      return null;
+    } on FirebaseAuthException catch (e, s) {
+      switch (e.code) {
+        case 'email-already-in-use':
+          log('MENSAGEM DE ERRO:', error: e, stackTrace: s);
+          return 'Este e-mail já está sendo utilizado';
+        case 'user-disabled':
+          log('MENSAGEM DE ERRO:', error: e, stackTrace: s);
+          return 'Esse usuário foi desabilitado';
+        case 'invalid-email':
+          return 'O endereço de e-mail não é válido';
+        case 'operation-not-allowed':
+          log('MENSAGEM DE ERRO:', error: e, stackTrace: s);
+          return 'Este aplicativo não foi configurado para autenticação por meio de e-mail e senha';
+        case 'weak-password':
+          log('MENSAGEM DE ERRO:', error: e, stackTrace: s);
+          return 'A senha digitada é muito fraca';
+        default:
+          log('MENSAGEM DE ERRO:', error: e, stackTrace: s);
+          return 'Erro não identificado';
+      }
+    }
+  }
+
   // Método para registrar usuário com email e senha
   Future<String?> signInWithEmail(
       {required String email, required String password}) async {
@@ -28,6 +61,7 @@ class FirebaseAuthService {
       }
     }
   }
+
   // Método para logout
   Future<void> signOut() async {
     await _auth.signOut();
