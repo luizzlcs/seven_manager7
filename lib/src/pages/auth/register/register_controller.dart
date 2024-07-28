7c9856c80d9c2508891900520eb88e7ab9c3a6ce
Luiz Carlos <luizzlcs@gmail.com>
2024-07-28 02:45:07 -0300

    Refatorando código.

--- lib/src/pages/auth/register/register_controller.dart
+++ lib/src/pages/auth/register/register_controller.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:seven_manager/src/core/services/firebase/firebase_auth_service.dart';
-import 'package:seven_manager/src/core/widgets/helpers/messages.dart';
+
+enum RegisterStatus {
+  initial,
+  loading,
+  success,
+  error;
+}
 
 class RegisterController with ChangeNotifier {
   RegisterController({
@@ -9,45 +15,43 @@ class RegisterController with ChangeNotifier {
 
   final FirebaseAuthService firebaseAuth;
 
+  RegisterStatus _registerStatus = RegisterStatus.initial;
+  RegisterStatus get registerStatus => _registerStatus;
+
   bool isVisible = false;
   bool passwordMatch = true;
+  String _message = '';
+  String get message => _message;
 
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-  final TextEditingController nameEC = TextEditingController();
-  final TextEditingController emailEC = TextEditingController();
-  final TextEditingController passwordEC = TextEditingController();
-  final TextEditingController confirmePasswordEC = TextEditingController();
-
-  
-  void changeVisible(){
+  void changeVisible() {
     isVisible = !isVisible;
     notifyListeners();
   }
 
-  void checkPasswordsMatch() {
-    
-    String password = passwordEC.text;
-    String confirmePassword = confirmePasswordEC.text;
-    passwordMatch = password == confirmePassword;
+  void checkPasswordsMatch(String password, String confirmPassword) {
+    passwordMatch = password == confirmPassword;
     notifyListeners();
   }
 
-  void createUser(BuildContext context)async{
-    String name = nameEC.text;
-    String email = emailEC.text;
-    String password = passwordEC.text;
-    final valid = formKey.currentState?.validate()?? false;
-    if(valid){
-    final String? auth = await firebaseAuth.createUser(email: email, password: password, name: name);
-      if(auth == null){
-        checkPasswordsMatch();
-        Messages.showInfo('Usuário Criado com suceso!', context);
-      }else{
-        checkPasswordsMatch();
-        Messages.showError(auth, context);
-      }
+  Future<void> createUser({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    _registerStatus = RegisterStatus.loading;
+    notifyListeners();
+    final String? auth = await firebaseAuth.createUser(
+        name: name, email: email, password: password);
 
+    if (auth == null) {
+      _message = 'Usuário criado com sucesso!';
+      notifyListeners();
+      _registerStatus = RegisterStatus.success;
+      notifyListeners();
+    } else {
+      _message = auth;
+      _registerStatus = RegisterStatus.error;
+      notifyListeners();
     }
-
   }
 }
