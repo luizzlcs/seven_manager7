47cb8da9e1c8031489cc53494c6127351d60eb48
Luiz Carlos <luizzlcs@gmail.com>
2024-07-29 18:08:37 -0300

    Removido uso desnecessários do get.

--- lib/src/pages/auth/register/register_controller.dart
+++ lib/src/pages/auth/register/register_controller.dart
@@ -1,39 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:seven_manager/src/core/services/firebase/firebase_auth_service.dart';
 
-enum RegisterStatus {
-  initial,
-  loading,
-  success,
-  error;
-}
+enum RegisterStatus { initial, loading, success, error }
 
 class RegisterController with ChangeNotifier {
-  RegisterController({
-    required this.firebaseAuth,
-  });
+  RegisterController({required this.firebaseAuth});
 
   final FirebaseAuthService firebaseAuth;
 
-  RegisterStatus _registerStatus = RegisterStatus.initial;
-  RegisterStatus get registerStatus => _registerStatus;
-
-  bool _isVisible = false;
-  bool get isVisible => _isVisible;
-
-  bool _passwordMatch = true;
-  bool get passwordMatch => _passwordMatch;
-
-  String _message = '';
-  String get message => _message;
+  RegisterStatus registerStatus = RegisterStatus.initial;
+  bool isVisible = false;
+  bool passwordMatch = true;
+  String message = '';
 
   void changeVisible() {
-    _isVisible = !_isVisible;
+    isVisible = !isVisible;
     notifyListeners();
   }
 
   void checkPasswordsMatch(String password, String confirmPassword) {
-    _passwordMatch = password == confirmPassword;
+    passwordMatch = password == confirmPassword;
     notifyListeners();
   }
 
@@ -42,19 +28,19 @@ class RegisterController with ChangeNotifier {
     required String email,
     required String password,
   }) async {
-    _registerStatus = RegisterStatus.loading;
+    registerStatus = RegisterStatus.loading;
     notifyListeners();
     final String? auth = await firebaseAuth.createUser(
         name: name, email: email, password: password);
 
     if (auth == null) {
-      _message = 'Usuário criado com sucesso!';
+      message = 'Você logu pela 1ª vez com $email';
       notifyListeners();
-      _registerStatus = RegisterStatus.success;
+      registerStatus = RegisterStatus.success;
       notifyListeners();
     } else {
-      _message = auth;
-      _registerStatus = RegisterStatus.error;
+      message = auth;
+      registerStatus = RegisterStatus.error;
       notifyListeners();
     }
   }
