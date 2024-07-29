0605c95715d6327d65ca8b65be7397609a30b10f
Luiz Carlos <luizzlcs@gmail.com>
2024-07-29 18:05:08 -0300

    Criado Injeção de dependência para recover controller.

--- lib/src/core/injection/injection.dart
+++ lib/src/core/injection/injection.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:get_it/get_it.dart';
 import 'package:seven_manager/src/pages/auth/login/login_controller.dart';
+import 'package:seven_manager/src/pages/auth/recoverPassword/recover_controller.dart';
 import 'package:seven_manager/src/pages/auth/register/register_controller.dart';
 import '../services/firebase/firebase_auth_service.dart';
 
@@ -11,10 +12,9 @@ void configureDependencies() {
   log('Inicio da configuração de dependência');
 
   getIt.registerFactory(() => FirebaseAuthService());
-  getIt.registerFactory(
-      () => RegisterController(firebaseAuth: getIt<FirebaseAuthService>()));
-  getIt.registerFactory(
-      () => LoginController(firebaseAuth: getIt<FirebaseAuthService>()));
+  getIt.registerFactory(() => RecoverController(firebaseAuth: getIt<FirebaseAuthService>()));
+  getIt.registerFactory(() => RegisterController(firebaseAuth: getIt<FirebaseAuthService>()));
+  getIt.registerFactory(() => LoginController(firebaseAuth: getIt<FirebaseAuthService>()));
 
   log('Fim da configuração de dependência');
 }
