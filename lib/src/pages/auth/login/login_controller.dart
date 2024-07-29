e2571f41a59eae519a7ae9af3b0740d97a838f5f
Luiz Carlos <luizzlcs@gmail.com>
2024-07-29 18:07:29 -0300

    Removido uso desnecessários do get.

--- lib/src/pages/auth/login/login_controller.dart
+++ lib/src/pages/auth/login/login_controller.dart
@@ -1,52 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:seven_manager/src/core/constants/app_router.dart';
 import 'package:seven_manager/src/core/services/firebase/firebase_auth_service.dart';
 
-import '../../../core/widgets/helpers/messages.dart';
-
-enum Status { none, error, sucess, loading }
+enum LoginStatus { initial, loading, success, error }
 
 class LoginController with ChangeNotifier {
-  LoginController({
-    required this.firebaseAuth,
-  });
+  LoginController({required this.firebaseAuth});
 
-  bool isVisible = false;
-  bool isLoading = false;
-  String statusMessage = '';
   final FirebaseAuthService firebaseAuth;
 
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-  final TextEditingController emailEC = TextEditingController();
-  final TextEditingController passwordEC = TextEditingController();
+  LoginStatus loginStatus = LoginStatus.initial;
+  bool isVisible = false;
+  String message = '';
 
   void changeVisible() {
     isVisible = !isVisible;
     notifyListeners();
   }
 
-  Future<void> login(BuildContext context) async {
-    final valid = formKey.currentState?.validate() ?? false;
-    String email = emailEC.text;
-    String password = passwordEC.text;
+  Future<void> login(String email, String password) async {
+    loginStatus = LoginStatus.loading;
+    notifyListeners();
+    final String? auth =
+        await firebaseAuth.signInWithEmail(email: email, password: password);
 
-    if (valid) {
-      isLoading = true;
+    if (auth == null) {
       notifyListeners();
-      final String? auth =
-          await firebaseAuth.signInWithEmail(email: email, password: password);
-
-      if (auth == null) {
-        Future.delayed(
-          const Duration(seconds: 1),
-          () => Navigator.of(context).pushNamed(AppRouter.homePage),
-        );
-      } else {
-        statusMessage = auth;
-        Messages.showError(auth, context);
-        isLoading = false;
-        notifyListeners();
-      }
+      loginStatus = LoginStatus.success;
+      notifyListeners();
+    } else {
+      message = auth;
+      loginStatus = LoginStatus.error;
+      notifyListeners();
     }
   }
 }
