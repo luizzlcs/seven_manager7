a0c4ceab0d27aef9d9be5ea346f4640f3150ad3a
Luiz Carlos <luizzlcs@gmail.com>
2024-08-01 15:11:23 -0300

    Criado visualização de imagem para o perfil do usuário com ImagePicker.

--- lib/src/pages/auth/register/pages/create_account_page.dart
+++ lib/src/pages/auth/register/pages/create_account_page.dart
@@ -1,15 +1,20 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:seven_manager/src/core/constants/app_images.dart';
 import 'package:seven_manager/src/core/constants/app_router.dart';
 import 'package:seven_manager/src/core/injection/injection.dart';
 import 'package:seven_manager/src/core/theme/seven_manager_theme.dart';
 import 'package:seven_manager/src/core/widgets/helpers/loader.dart';
 import 'package:seven_manager/src/core/widgets/helpers/messages.dart';
+import 'package:seven_manager/src/core/widgets/imageProvider/image_profile_controller.dart';
 import 'package:seven_manager/src/pages/auth/register/register_controller.dart';
 import 'package:validatorless/validatorless.dart';
 
 import '../../../../core/widgets/helpers/seven_loader.dart';
 import '../../../../core/widgets/imageProvider/image_provider_widget.dart';
+import '../../../../core/widgets/imageProvider/modal_bottom_sheet.dart';
 
 class CreateAccountPage extends StatefulWidget {
   const CreateAccountPage({super.key});
@@ -130,8 +135,8 @@ class _CreateAccountPageState extends State<CreateAccountPage> with Loader {
                         ),
                       ),
                       const SizedBox(height: 10),
-                      
-                      const ImageLogoWidget(pathImage: AppImages.logoIasd),
+                      ImageLogoWidget(),
+                      // const ImageLogoWidget(pathImage: AppImages.logoIasd),
                       const SizedBox(height: 10),
                       TextFormField(
                         controller: nameEC,
