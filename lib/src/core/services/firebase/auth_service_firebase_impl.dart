d6bfb12336b670844d308ffa90007bda47b5e3f3
Luiz Carlos <luizzlcs@gmail.com>
2024-08-07 21:18:23 -0300

    Implementado método para criar conta e colection de pessoas e Igrejas.

--- lib/src/core/services/firebase/auth_service_firebase_impl.dart
+++ lib/src/core/services/firebase/auth_service_firebase_impl.dart
@@ -1,45 +1,112 @@
 import 'dart:developer';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:seven_manager/src/core/services/firebase/cloud_firestore.dart';
+import 'package:seven_manager/src/model/churchs_model.dart';
+import 'package:seven_manager/src/model/persons_model.dart';
+
 
 class AuthServiceFirebaseImpl {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  AuthServiceFirebaseImpl({
+    required FirebaseAuth auth,
+  }) : _auth = auth;
 
- 
+  final FirebaseAuth _auth;
 
-  
+  CloudFirestore _cloudFirestore = CloudFirestore('churchs');
+  // final FirebaseFirestore;
+
   Future<String?> createUser({
-    required String name,
+    required String namePerson,
+    required String dateOfBirthPerson,
+    required String cpf,
+    required String malePerson,
+    required String whastAppPerson,
+    required String numberPerson,
+    required String cityPerson,
+    required String zipCodePerson,
+    required String statePerson,
+    required String streetPerson,
+    required bool isPostalServicePerson,
     String? photoURL,
-    required String email,
+    required String emailPerson,
     required String password,
+    required String districtChuchs,
+    required String cityChuchs,
+    required String zipCodeChuchs,
+    required String streetChuchs,
+    required String stateChuchs,
   }) async {
-    try {
-      UserCredential userCredential = await _auth
-          .createUserWithEmailAndPassword(email: email, password: password);
-      await userCredential.user!.updateDisplayName(name);
-      await userCredential.user!.updatePhotoURL(photoURL);
-      return null;
-    } on FirebaseAuthException catch (e, s) {
-      switch (e.code) {
-        case 'email-already-in-use':
-          log('MENSAGEM DE ERRO:', error: e, stackTrace: s);
-          return 'Este e-mail já está sendo utilizado';
-        case 'user-disabled':
-          log('MENSAGEM DE ERRO:', error: e, stackTrace: s);
-          return 'Esse usuário foi desabilitado';
-        case 'invalid-email':
-          return 'O endereço de e-mail não é válido';
-        case 'operation-not-allowed':
-          log('MENSAGEM DE ERRO:', error: e, stackTrace: s);
-          return 'Este aplicativo não foi configurado para autenticação por meio de e-mail e senha';
-        case 'weak-password':
-          log('MENSAGEM DE ERRO:', error: e, stackTrace: s);
-          return 'A senha digitada é muito fraca';
-        default:
-          log('MENSAGEM DE ERRO:', error: e, stackTrace: s);
-          return 'Erro não identificado';
+    // Cria o usuário no Firebase Authentication
+    UserCredential userCredential = await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: emailPerson, password: password);
+
+    // Obtém
+    //  o UID do usuário recém-criado
+    String uid = userCredential.user!.uid;
+
+    // Transação para garantir a consistência dos dados
+    await FirebaseFirestore.instance
+        .runTransaction((Transaction transaction) async {
+      final firestoreService = CloudFirestore('churchs');
+      final churchName = districtChuchs;
+
+      // Verifica se a igreja já existe
+      QuerySnapshot querySnapshot = await firestoreService.getByField(
+          fieldName: districtChuchs, value: districtChuchs);
+
+      String idNewChurch = '';
+
+      // Verifica se a igreja já existe
+      if (querySnapshot.docs.isNotEmpty) {
+        DocumentSnapshot documentSnapshot = querySnapshot.docs.first;
+        log('Documento encontrado: ${documentSnapshot.data()}');
+      } else {
+        log('Nenhum documento encontrado.');
+        log('Criando novo documento...');
+
+        // Cria o documento da igreja
+        final newChurchs = ChurchsModel(
+          districtChuchs: districtChuchs,
+          cityChuchs: cityChuchs,
+          zipCodeChuchs: zipCodeChuchs,
+          streetChuchs: streetChuchs,
+          stateChuchs: stateChuchs,
+        );
+
+        DocumentReference docRef =
+            await _cloudFirestore.create(data: newChurchs.toMap());
+
+        idNewChurch = docRef.id;
+
+        log('Novo documento criado com sucesso ID: $docRef.');
       }
-    }
+
+      // _cloudFirestore = CloudFirestore('persons');
+
+      final newPerson = PersonsModel(
+        idChurch: uid, 
+        malePerson: malePerson, 
+        namePerson: namePerson, 
+        dateOfBirthPerson: dateOfBirthPerson, 
+        cpf: cpf, 
+        emailPerson: emailPerson, 
+        whastAppPerson: whastAppPerson, 
+        zipCodePerson: zipCodePerson, 
+        numberPerson: numberPerson, 
+        cityPerson: cityPerson, 
+        statePerson: statePerson, 
+        isPostalServicePerson: isPostalServicePerson,
+        streetPerson: streetPerson,
+        );
+
+      // Cria o documento do usuário na coleção "pessoas"
+
+      await _cloudFirestore.createDocumentWithSpecificId(
+          'persons', uid, newPerson.toMap());
+      // await _cloudFirestore.create(data: newPerson.toMap());
+    });
+    return null;
   }
 
   // Método para registrar usuário com email e senha
