83c74694888548da30616c6d7b0e7740b6fd9bab
Luiz Carlos <luizzlcs@gmail.com>
2024-08-17 23:40:01 -0300

    Adicionado campo de data de crualçai ao modelo

--- lib/src/model/churchs_model.dart
+++ lib/src/model/churchs_model.dart
@@ -1,14 +1,15 @@
 import 'dart:convert';
 
 class ChurchsModel {
-  const ChurchsModel({
+  ChurchsModel({
     this.idChuchs,
     required this.districtChuchs,
     required this.cityChuchs,
     required this.zipCodeChuchs,
     required this.streetChuchs,
     required this.stateChuchs,
-  });
+    String? creationDate,
+  }) : creationDate = DateTime.now().toIso8601String();
 
   final String? idChuchs;
   final String zipCodeChuchs;
@@ -16,8 +17,7 @@ class ChurchsModel {
   final String districtChuchs;
   final String cityChuchs;
   final String stateChuchs;
-
-  
+  final String creationDate;
 
   Map<String, dynamic> toMap() {
     return {
@@ -27,6 +27,7 @@ class ChurchsModel {
       'districtChuchs': districtChuchs,
       'cityChuchs': cityChuchs,
       'stateChuchs': stateChuchs,
+      'creationDate': creationDate,
     };
   }
 
@@ -38,12 +39,12 @@ class ChurchsModel {
       districtChuchs: map['districtChuchs'] ?? '',
       cityChuchs: map['cityChuchs'] ?? '',
       stateChuchs: map['stateChuchs'] ?? '',
+      creationDate: map['creationDate']?? '',
     );
   }
 
   String toJson() => json.encode(toMap());
 
-  factory ChurchsModel.fromJson(String source) => ChurchsModel.fromMap(json.decode(source));
+  factory ChurchsModel.fromJson(String source) =>
+      ChurchsModel.fromMap(json.decode(source));
 }
-
-
