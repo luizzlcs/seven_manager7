45e887008c80a576b33d2a8f85e6b12b7a162f37
Luiz Carlos <luizzlcs@gmail.com>
2024-08-23 10:51:04 -0300

    Adição e remoção de imagens

--- lib/src/core/constants/app_images.dart
+++ lib/src/core/constants/app_images.dart
@@ -3,6 +3,5 @@ abstract class AppImages {
   static const logoGApp = 'assets/images/logoG_seven_manager.png';
   static const logoIasd = 'assets/images/LogoIgreja.png';
   static const avatar = 'assets/images/avatar.png';
-  static const correios = 'assets/images/correios.png';
-  static const correiosLogo = 'assets/images/logo_correios.png';
+  static const map = 'assets/images/map.png';
 }
