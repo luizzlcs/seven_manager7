52a473b5b4534e4c8cedd7c3845ba4f3e12d42fb
Luiz Carlos <luizzlcs@gmail.com>
2024-08-23 10:54:36 -0300

    Adicionado controller de imagens.

--- lib/src/core/injection/injection.dart
+++ lib/src/core/injection/injection.dart
@@ -4,13 +4,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 import 'package:seven_manager/src/core/services/dio_http_client.dart';
 import 'package:seven_manager/src/core/services/firebase/firebase_storage_service.dart';
-import 'package:seven_manager/src/core/widgets/imageAvatar/image_profile_controller.dart';
+import 'package:seven_manager/src/pages/auth/register/pages/aboutYouchurchPage/image_profile_controller_church.dart';
 import 'package:seven_manager/src/interfaces/http_client.dart';
-import 'package:seven_manager/src/model/cep_model.dart';
 import 'package:seven_manager/src/pages/auth/login/login_controller.dart';
 import 'package:seven_manager/src/pages/auth/recoverPassword/recover_controller.dart';
 import 'package:seven_manager/src/pages/auth/register/pages/aboutYouPage/cep_controller_you.dart';
 import 'package:seven_manager/src/pages/auth/register/pages/aboutYouchurchPage/cep_controller_you_church.dart';
+import 'package:seven_manager/src/pages/auth/register/pages/createAccountPage/image_profile_controller_account.dart';
 import 'package:seven_manager/src/pages/auth/register/register_controller.dart';
 import 'package:seven_manager/src/repositories/cep_repository.dart';
 import '../services/firebase/auth_service_firebase_impl.dart';
@@ -28,7 +28,8 @@ void configureDependencies() {
   getIt.registerLazySingleton<RecoverController>(() => RecoverController(firebaseAuth: getIt<AuthServiceFirebaseImpl>()));
   getIt.registerLazySingleton<RegisterController>(() => RegisterController(firebaseAuth: getIt<AuthServiceFirebaseImpl>()));
   getIt.registerLazySingleton<LoginController>(() => LoginController(firebaseAuth: getIt<AuthServiceFirebaseImpl>()));
-  getIt.registerLazySingleton<ImageProfileController>(() => ImageProfileController(storage: getIt<FirebaseStorageService>()));
+  getIt.registerLazySingleton<ImageProfileControllerChurch>(() => ImageProfileControllerChurch(storage: getIt<FirebaseStorageService>()));
+  getIt.registerLazySingleton<ImageProfileControllerAccount>(() => ImageProfileControllerAccount(storage: getIt<FirebaseStorageService>()));
   getIt.registerLazySingleton<CepControllerYou>(() => CepControllerYou(cepRepository: getIt<CepRepository>()));
   getIt.registerLazySingleton<CepControllerYouChurch>(() => CepControllerYouChurch(cepRepository: getIt<CepRepository>()));
 
