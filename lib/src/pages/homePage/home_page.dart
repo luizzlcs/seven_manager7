4bbdbc8b2c3942aa8a01c2ded507c8a8258706d2
Luiz Carlos <luizzlcs@gmail.com>
2024-09-12 09:21:00 -0300

    criado página inicial

--- lib/src/pages/homePage/home_page.dart
+++ lib/src/pages/homePage/home_page.dart
@@ -1,31 +1,137 @@
 import 'package:flutter/material.dart';
-import 'package:seven_manager/src/core/constants/app_router.dart';
-import 'package:seven_manager/src/core/injection/injection.dart';
-import 'package:seven_manager/src/core/services/firebase/auth_service_firebase_impl.dart';
+import 'package:seven_manager/src/pages/homePage/widgets/custom_avatar.dart';
+import '../../core/theme/seven_manager_theme.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-  const HomePage({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-   AuthServiceFirebaseImpl auth = getIt();
-       return Scaffold(
-           appBar: AppBar(title: const Text('Home Page'),),
-           body: Center(
-             child: Column(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.menu,
+            size: 30,
+            color: SevenManagerTheme.whiteColor,
+          ),
+        ),
+        backgroundColor: SevenManagerTheme.tealBlue,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.favorite,
+              size: 30,
+              color: SevenManagerTheme.whiteColor,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.more_vert,
+              size: 30,
+              color: SevenManagerTheme.whiteColor,
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  color: Colors.white,
-                  child: IconButton(onPressed: (){
-                    auth.signOut();
-                    Navigator.of(context).pushReplacementNamed(AppRouter.login);
-                  }, icon: const Icon(Icons.logout)),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'Olá, Rodrigo',
+                    style: TextStyle(
+                        color: SevenManagerTheme.whiteColor,
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(
+                  width: 110,
                 )
               ],
-             ),
-           ),
-       );
+            ),
+            Center(
+              child: Stack(
+                children: [
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      height: 715,
+                      width: double.infinity,
+                      decoration: const ShapeDecoration(
+                        color: SevenManagerTheme.whiteColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20.0),
+                            topRight: Radius.circular(20.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding:  EdgeInsets.all(0.0),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 10,
+                        ),
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            children: [
+                             CustomCircleAvatar(),
+                             CustomCircleAvatar(),
+                             CustomCircleAvatar(),
+                             CustomCircleAvatar(),
+                             CustomCircleAvatar(),
+                             CustomCircleAvatar(),
+                             CustomCircleAvatar(),
+                             CustomCircleAvatar(),
+                             CustomCircleAvatar(),
+                             CustomCircleAvatar(),
+                              
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+  //  Widget build(BuildContext context) {
+  //  AuthServiceFirebaseImpl auth = getIt();
+  //      return Scaffold(
+  //          appBar: AppBar(title: const Text('Home Page'),),
+  //          body: Center(
+  //            child: Column(
+  //             mainAxisAlignment: MainAxisAlignment.center,
+  //             children: [
+  //               Container(
+  //                 color: Colors.white,
+  //                 child: IconButton(onPressed: (){
+  //                   auth.signOut();
+  //                   Navigator.of(context).pushReplacementNamed(AppRouter.login);
+  //                 }, icon: const Icon(Icons.logout)),
+  //               )
+  //             ],
+  //            ),
+  //          ),
+  //      );
+  // }
