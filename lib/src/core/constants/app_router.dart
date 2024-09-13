eb13e8d2da6bdd03f6d35dd3fac1ec27887beb84
Luiz Carlos <luizzlcs@gmail.com>
2024-09-13 14:21:38 -0300

    Removido código desnecessário.

--- lib/src/core/constants/app_router.dart
+++ lib/src/core/constants/app_router.dart
@@ -3,7 +3,6 @@ import 'package:seven_manager/src/pages/auth/login/login_page.dart';
 import 'package:seven_manager/src/pages/homePage/home_page.dart';
 import 'package:seven_manager/src/pages/splashPage/splash_page.dart';
 
-import '../../../router_page.dart';
 import '../../pages/auth/recoverPassword/recover_password_page.dart';
 import '../../pages/auth/register/register_page.dart';
 
@@ -33,7 +32,6 @@ abstract class AppRouter {
   static Map<String, WidgetBuilder> get router {
     return {
       '/': (BuildContext context) => const SplashPage(),
-      routerPage: (BuildContext context) => const RouterPage(),
       login: (BuildContext context) => const LoginPage(),
       register: (BuildContext context) => const RegisterPage(),
       recover: (BuildContext context) => const RecoverPasswordPage(),
@@ -41,7 +39,6 @@ abstract class AppRouter {
     };
   }
 
-  static const routerPage = '/router-page';
   static const login = '/login';
   static const register = '/register';
   static const recover = '/recover';
