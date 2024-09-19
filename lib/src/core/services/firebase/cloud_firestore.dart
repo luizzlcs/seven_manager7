b35833657ba1eca6a94b51ecaa145d04d8deb92d
Luiz Carlos <luizzlcs@gmail.com>
2024-09-18 21:54:50 -0300

    Ajustado os métodos de  CRUD do FireStorage.

--- lib/src/core/services/firebase/cloud_firestore.dart
+++ lib/src/core/services/firebase/cloud_firestore.dart
@@ -1,45 +1,50 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class CloudFirestore {
-  CloudFirestore(String collection)
-      : _collection = FirebaseFirestore.instance.collection(collection);
+  static final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  final CollectionReference _collection;
-
-  CollectionReference get instance => _collection;
-
-  Future<QuerySnapshot> getAll({required Query query}) {
-    return query.get();
+  static Future<QuerySnapshot> getAll(
+      {required String collection, required Query query}) async {
+    return _firestore.collection(collection).where(query).get();
   }
 
-  Future<DocumentSnapshot> get({required String documentId}) {
-    return _collection.doc(documentId).get();
+  Future<DocumentSnapshot> get(
+      {required String collection, required String documentId}) {
+    return _firestore.collection(collection).doc(documentId).get();
   }
 
-  void delete({required String documentId}) {
-    _collection.doc(documentId).delete();
+  void delete({required String collection, required String documentId}) {
+    _firestore.collection(collection).doc(documentId).delete();
   }
 
   Future<void> update(
-      {required String documentId, required Map<String, dynamic> data}) {
-    return _collection.doc(documentId).set(data);
+      {required String collection,
+      required String documentId,
+      required Map<String, dynamic> data}) {
+    return _firestore.collection(collection).doc(documentId).set(data);
   }
 
-  Future<DocumentReference> create({required Map<String, dynamic> data}) {
-    return _collection.add(data);
+  Future<DocumentReference> create(
+      {required String collection, required Map<String, dynamic> data}) {
+    return _firestore.collection(collection).add(data);
   }
 
   Future<QuerySnapshot> getByField(
-      {required String fieldName, required dynamic value}) {
-    return _collection.where(fieldName, isEqualTo: value).limit(1).get();
+      {required String collection,
+      required String fieldName,
+      required dynamic value}) {
+    return _firestore
+        .collection(collection)
+        .where(fieldName, isEqualTo: value)
+        .limit(1)
+        .get();
   }
 
-  Future<void> createDocumentWithSpecificId(
-    String collectionName,
-    String documentId,
-    Map<String, dynamic> data,
-  ) async {
-    final collection = FirebaseFirestore.instance.collection(collectionName);
-    await collection.doc(documentId).set(data);
+  Future<void> createDocumentWithSpecificId({
+    required String collection,
+    required String documentId,
+    required Map<String, dynamic> data,
+  }) async {
+    await _firestore.collection(collection).doc(documentId).set(data);
   }
 }
