5d43516a0bd6ec8bb0d158c071bfc07ff6a016fe
Luiz Carlos <luizzlcs@gmail.com>
2024-09-30 15:54:51 -0300

    Removido # da url.

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:seven_manager/firebase_options.dart';
 import 'package:seven_manager/seven_manager.dart';
 import 'package:seven_manager/src/core/injection/injection.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  usePathUrlStrategy();
+
 
   await FirebaseAppCheck.instance.activate(
     webProvider: ReCaptchaV3Provider('recaptcha-v3-site-key'),
