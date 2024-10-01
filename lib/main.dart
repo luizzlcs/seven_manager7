9f567822cec555527f4733e0725d4df4874e1369
Luiz Carlos <luizzlcs@gmail.com>
2024-10-01 12:15:48 -0300

    Usando setPathUrlStrategy para web

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
+import 'dart:developer';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:seven_manager/firebase_options.dart';
 import 'package:seven_manager/seven_manager.dart';
 import 'package:seven_manager/src/core/injection/injection.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  usePathUrlStrategy();
 
+  checkPlataform();
 
   await FirebaseAppCheck.instance.activate(
     webProvider: ReCaptchaV3Provider('recaptcha-v3-site-key'),
@@ -20,4 +22,13 @@ void main() async {
 
   configureDependencies();
   runApp(const SevenManager());
+}
+
+void checkPlataform() {
+  if (kIsWeb) {
+    log('Executando na WEB');
+    setPathUrlStrategy();
+  } else {
+    log('Executando no Mobile ou Desktop');
+  }
 }
